9a6f90c07c98d68a7e18da71d12ec61ce1704bd1
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-03-30 16:56:09 -0500

    finished

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-let x = 10;
-let y = 10;
-let result = x + y;
-console.log(result);
+var win = 0;
+var lose = 0;
+var tie = 0;
+
+var game = ["R", "P", "S"];
+
+function play() {
+    var choice = prompt("Let's play a game! \nRock, Paper, Scissors? \nPlease enter R, P, or S.");
+    var chosen = choice.toUpperCase();
+    var index = Math.floor(Math.random(game) * game.length);
+    var cpu = game[index];
+    alert("You chose " + chosen + " & The Internet chose: " + cpu);
+
+    if (chosen === null) {
+        return play();     
+    }   
+    if (chosen === cpu) {
+            alert("It's a tie!");
+            tie++;
+    }   
+    else if (chosen === "P" && cpu === "R" || 
+            chosen === "R" && cpu === "S" || 
+            chosen === "S" && cpu === "P") {
+            alert("You Win!");
+            win++;
+    } else {
+        alert("You Lose!");
+        lose++;
+    }
+    alert("Status: \nWin: " + win + "\nLose: " + lose + "\nTie: " + tie);
+    let playAgain = confirm("Play Again?");
+
+    if (playAgain === true){
+        return play();
+    }
+}
+
+play();
